b6490df8225a919c77ffde952ae89dee4654941f
Pitchumani Sivanupandi <s.pitchumani@gmail.com>
2025-08-11 14:08:14 -0400

    Added mutex lock to access activites data in server

--- activity-log/internal/server/activity.go
+++ activity-log/internal/server/activity.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"fmt"
+	"log"
+	"sync"
 
 	api "github.com/pitchumani/activity-tracker/activity-log"
 )
@@ -9,13 +11,17 @@ import (
 // define activity info for the server package
 
 type Activities struct {
+	mtx         sync.Mutex
 	activities  []api.Activity
 }
 
 // define a insert method for Activities type
 func (acts *Activities) Insert(activity api.Activity) int {
+	acts.mtx.Lock()
+	defer acts.mtx.Unlock()
 	activity.ID = len(acts.activities) + 1
 	acts.activities = append(acts.activities, activity)
+	log.Printf("Added %v\n", activity)
 	return activity.ID
 }
 
@@ -23,10 +29,13 @@ var ErrIDNotFound = fmt.Errorf("ID not found")
 
 // define a retrieve method for Activities type
 func (acts *Activities) Retrieve(id int) (api.Activity, error) {
+	log.Printf("Getting id %d\n", id)
+	acts.mtx.Lock()
+	defer acts.mtx.Unlock()
 	if id > len(acts.activities) || id == 0 {
+		log.Printf("ID %d is not found\n", id)
 		return api.Activity{}, ErrIDNotFound
 	}
 	activity := acts.activities[id-1]
 	return activity, nil
 }
-
